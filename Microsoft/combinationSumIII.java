7ee3a11576927052182547f35dc0c42ce4c8486a
Madhu Shaw <59835523+madhushaw1012@users.noreply.github.com>
2023-01-03 13:53:21 +0530

    Added solution

--- Microsoft/combinationSumIII.java
+++ Microsoft/combinationSumIII.java
@@ -1,5 +1,18 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        
+        List<List<Integer>> res= new ArrayList<>();
+        f(1,n,k, res, new ArrayList<>());
+        return res;
+    }
+    public void f(int ind, int sum, int k, List<List<Integer>> res, List<Integer> list) {
+        if(sum == 0) {
+            if(list.size()==k) res.add(new ArrayList<>(list));
+            return;
+        }
+        if(ind > sum || ind >9) return;
+        list.add(ind);
+        f(ind+1, sum-ind,k,res,list);
+        list.remove(list.size()-1);
+        f(ind+1, sum, k, res, list);
     }
 }
